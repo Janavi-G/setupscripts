75cd690b8d979b82207044265b66a44c21da9a6b
Vijayvithal Jahagirdar <jvs@dyumnin.com>
2022-12-29 22:13:30 +0530

    Fixed syntax errors in setup file

--- setup.sh
+++ setup.sh
@@ -2,17 +2,17 @@
 #
 apt install git curl
 #Install Vim plugins
-if [! -f ~/.vimrc]; then
+if [[ ! -f ~/.vimrc ]]; then
 	# Install default set of plugins and vimrc
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	cp vimrc ~/.vimrc
 	pip3 install rope ropevim
-	vim -c PluginInstall
+	vim -c PluginInstall -c ":qall"
 	# install rope
 fi
 
 # Install Prezto
-if [! -f ~/.zshrc]; then
+if [[ ! -f ~/.zshrc ]]; then
 	git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 	setopt EXTENDED_GLOB
 	for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
@@ -21,7 +21,7 @@ if [! -f ~/.zshrc]; then
 fi
 
 #install Tmux
-if [! -f ~/.tmux.conf]; then
-	cp tmux.conf ~/.tmux.conf
+if [[ ! -f ~/.tmux.conf ]]; then
+	cp tmuxconf ~/.tmux.conf
 fi
 
