44cd49054e28ccfdb9323be371517abb55145db1
Vijayvithal <jahagirdar.vs@gmail.com>
2023-02-12 16:40:44 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [[ ! -f ~/.vimrc ]]; then
 	# Install default set of plugins and vimrc
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	cp vimrc ~/.vimrc
-	pip3 install rope ropevim
+	pip3 install rope ropevim autopep8 flake8
 	vim -c PluginInstall -c ":qall"
         cd .vim/bundle/YouCompleteMe && python3 install.py
 	# install rope
